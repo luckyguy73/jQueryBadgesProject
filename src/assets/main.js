4ddd627401fa33fae296750af43c0ea78a254bfd
ashton morgan <luckyguy73@yahoo.com>
2017-03-04 15:30:21 -0800

    completed build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/2787214.json',
+        dataType: 'jsonp',
+        success: function(data) {
+            showCourses(data.courses.completed)
+        }
+    });
+
+    function showCourses(courses){
+        var $badges = $('#badges');
+
+        courses.forEach(function(course) {
+
+            $div = $('<div />', {
+                'class': 'course'
+            }).appendTo($badges);
+
+            $('<h3 />', {
+                text: course.title
+            }).appendTo($div);
+
+            $('<img />', {
+                src: course.badge
+            }).appendTo($div);
+
+            $('<a />', {
+                'class': 'btn btn-primary',
+                target: '_blank',
+                href: course.url,
+                text: 'See Course'
+            }).appendTo($div);
+
+        });
+    }
 
 });
